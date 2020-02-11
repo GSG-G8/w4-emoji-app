96f1a6018a52c961b4c638dd5852a9102f761269
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-02-11 16:29:31 +0200

    handle other file relates #11

--- src/router.js
+++ src/router.js
@@ -1,6 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 
+const fileExtention = {
+  html: 'text/html',
+  css: 'text/css',
+  jpg: 'image/jpeg"',
+  png: 'image/png"',
+  ico: 'image/x-ion',
+  js: 'text/javascript',
+};
+
 const router = (request, response) => {
   const endpoint = request.url;
   if (endpoint === '/') {
@@ -11,6 +20,20 @@ const router = (request, response) => {
         response.end('<h1>500 error</h1>');
       } else {
         response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end(file);
+      }
+    });
+  } else if (endpoint.includes('public')) {
+    const extention = endpoint.split('.')[1];
+    const fileName = endpoint.split('/');
+    const filePath = path.join(__dirname, '..', ...fileName);
+    fs.readFile(filePath, (error, file) => {
+      if (error) {
+        console.log(error);
+        response.writeHead(500, { 'Content-Type': 'text/html' });
+        response.end('<h1>500 error</h1>');
+      } else {
+        response.writeHead(200, { 'Content-Type': fileExtention[extention] });
         response.end(file);
       }
     });
