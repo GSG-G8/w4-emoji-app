f71dff1abfc92d89756353699f743b41d0448967
Hassan Al-najjar <ha77an19@gmail.com>
2020-02-11 15:12:14 +0200

    xhr.js #10

---
+++ public/javaScript/xhr.js
@@ -0,0 +1,17 @@
+const xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = () => {
+  if (xhr.readyState === 4) {
+    if (xhr.status === 200) {
+      try {
+          JSON.parse(xhr.responseText);
+      } catch (error) {
+       console.log(error);
+      }
+    } else if (xhr.status === 404) {
+      console.log(xhr.responseText);
+    }
+  }
+};
+xhr.open('GET', 'url in here', true);
+xhr.send();
