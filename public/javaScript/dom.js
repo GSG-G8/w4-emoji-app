d1f76b8d7181d27c90edf8ad9b6739fa88067517
omar-dahdouh <omar.m.dahdouh@gmail.com>
2020-02-12 15:13:48 +0200

    renderAutocomplete function relates #21

--- public/javaScript/dom.js
+++ public/javaScript/dom.js
@@ -1,8 +1,8 @@
 
 let selected = 0;
 const suggestionsCount = 6;
-
 const sugestions = [];
+
 const input = document.getElementById('search-input');
 
 input.addEventListener('keydown', (evn) => {
@@ -20,7 +20,21 @@ input.addEventListener('keydown', (evn) => {
   }
 });
 
-input.addEventListener('keyup', (evn) => {
-    xhrSend();
+input.addEventListener('keyup', () => {
+  xhrSend();
 });
 
+function renderAutocomplete() {
+  let div;
+  const list = document.getElementById('suggestion');
+
+  list.textContent = '';
+  sugestions.forEach((sug, idx) => {
+    div = document.createElement('li');
+    div.textContent = `${sugestions[idx].char} ${sugestions[idx].name}`;
+    if (selected === idx) div.classList.add('active');
+    list.appendChild(div);
+  });
+}
+
+renderAutocomplete();
