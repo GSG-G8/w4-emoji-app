f53d0d95eddaf8084568e27d860ddddcc42b3f26
Hassan Al-najjar <ha77an19@gmail.com>
2020-02-12 11:17:55 +0200

    error handel #26

--- src/router.js
+++ src/router.js
@@ -8,5 +8,8 @@ module.exports = (req, res) => {
     homehandle(res);
   } else if (endpoint.includes('public')) {
     publichandle(req, res);
+  } else {
+    res.writeHead(500, { 'Conten-Type': 'text/html' });
+    res.end('<h1 style="color:black; font-weight:bold; border:2px solid #20ff; padding:20px; margin:20%; text-align:center;"> Error From Server  <span style="color:red;">500</span></h1>');
   }
 };
