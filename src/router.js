bf4a2e4329c6937cfb4397ef71b71e66a7edd513
Ali <ali.dahdouh@gmail.com>
2020-02-12 14:47:12 +0200

    added 404 file relates #34

--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 const searchHandle = require('./handelers/searchHandle');
 const publicHandle = require('./handelers/publichandle');
 
@@ -17,7 +18,10 @@ module.exports = (request, response) => {
   } else if (endpoint[0] === 'search') {
     searchHandle(request, response, endpoint[1]);
   } else {
-    response.writeHead(404, { 'Conten-Type': 'text/html' });
-    response.end(`<h1 style='color:black; font-weight:bold; border:2px solid #20ff; padding:20px; margin:20%; text-align:center;'> Page not found <span style='color:red;'>404</span> <p><a href='http://${HOST}:${PORT}' style='background-color: #2D65C9; padding:5px; border: 1px solid black; border-radius:10px; cursor: pointer; text-decoration: none; color:black;'>back To Home</a></h1>`);
+    fs.readFile(path.join(__dirname, '..', 'public', '404.html'), (err, file) => {
+      response.writeHead(404, { 'Content-Type': 'text/html' });
+      
+      response.end(file);
+    });
   }
 };
